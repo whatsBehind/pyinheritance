6630297edf657507c15977e3c916d853bfd8657b
puyanh <puyanh18@gmail.com>
2024-06-09 17:05:35 -0700

    correct test class name in class_node_test

--- test/PythonInheritance/class_node_test.py
+++ test/PythonInheritance/class_node_test.py
@@ -1,32 +1,32 @@
 from PythonInheritance.class_node import *
 
 
-class base_class_1:
+class BaseClass1:
     def hello_from_base_class_1(self):
         pass
 
 
-class base_class_2:
+class BaseClass2:
     def hello_from_base_class_2(self):
         pass
 
 
-class some_class(base_class_1, base_class_2):
+class SomeClass(BaseClass1, BaseClass2):
     def hello_from_some_class(self):
         pass
 
 
-some_class_node = ClassNode(some_class)
+some_class_node = ClassNode(SomeClass)
 
 
 def test_get_unique_class_name__correct_name():
-    result = get_unique_class_name(some_class)
+    result = get_unique_class_name(SomeClass)
     expected = "class_node_test.some_class"
     assert result == expected, f"Expected {expected}, got {result}"
 
 
 def test_get_all_methods__correct_list():
-    result = sorted(get_all_methods(some_class))
+    result = sorted(get_all_methods(SomeClass))
     expected = sorted(["hello_from_base_class_1", "hello_from_base_class_2", "hello_from_some_class"])
     assert result == expected, f"Expected {expected}, got {result}"
 
@@ -39,5 +39,5 @@ def test_class_node__correct_distinct_methods():
 
 def test_class_node__correct_base_class_names():
     result = sorted(some_class_node.base_classes.keys())
-    expected = sorted([get_unique_class_name(base_class_1), get_unique_class_name(base_class_2)])
+    expected = sorted([get_unique_class_name(BaseClass1), get_unique_class_name(BaseClass2)])
     assert result == expected, f"Expected {expected}, got {result}"
